c1fe9136ba699d1a193b984b1271310e13b09f9a
xiaoranzhou <zxrzxr@gmail.com>
2023-02-28 10:53:49 +0100

    update

---
+++ wasm/debug/checklist/ERCs/hn.js
@@ -0,0 +1,23 @@
+const login = require("./login.json");
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://wwwdev.ebi.ac.uk/ena/submit/webin/login', {
+    waitUntil: 'networkidle2',
+	
+  });
+  await page.type('[name="username"]', login["wwwdev.ebi.ac.uk/ena"]["username"]);
+  await page.type('[name="password"]', login["wwwdev.ebi.ac.uk/ena"]["password"]);
+    await Promise.all([
+    page.click('[name="action"]'),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+	page.click('[name="mat-caption"]'),
+	page.waitForNavigation({ waitUntil: 'networkidle0' }),
+	
+  ]);	
+  await page.pdf({ path: 'hn.pdf', format: 'a4' });
+  await browser.close();
+})();
